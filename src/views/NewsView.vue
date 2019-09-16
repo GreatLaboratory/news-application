21c1e3abd8bca7673763164d8f9d75e513ee1898
Myung-gwan Kim <cartopia95@naver.com>
2019-09-16 17:20:44 +0900

    toolbar, news, ask, jobs 컴포넌트 생성

---
+++ src/views/NewsView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div v-for="user in users" :key="user.id">{{ user }}</div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: "NewsView",
+        data() {
+            return {
+                users : []
+            }
+        },
+        created() {
+            axios.get('https://api.hnpwa.com/v0/news/1.json')
+                .then((res)=>{
+                    console.log(res);
+                    this.users = res.data;
+            })
+                .catch((err)=>{
+                    console.log(err)
+                })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
