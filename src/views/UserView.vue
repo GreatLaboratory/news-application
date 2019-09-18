6b9b9f475169118a479d133356ccb7183cf0c702
Myung-gwan Kim <cartopia95@naver.com>
2019-09-18 22:19:02 +0900

    사용자정보 페이지 뷰 추가

---
+++ src/views/UserView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="user">
+        <p>name : {{ user.id }}</p>
+        <p>karma : {{ user.karma }}</p>
+        <p>created : {{ user.created }}</p>
+    </div>
+</template>
+
+<script>
+    import { mapActions, mapState } from 'vuex'
+    export default {
+        name: "UserView",
+        computed: {
+            ...mapState([
+                'user'
+            ])
+        },
+        methods: {
+            ...mapActions([
+                'FETCH_USER'
+            ])
+        },
+        created() {
+            const userName = this.$route.params.id;
+            this.FETCH_USER(userName)
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
