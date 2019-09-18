5a24e81f63b3a82e5aff43899eabe9065315e347
Myung-gwan Kim <cartopia95@naver.com>
2019-09-18 22:13:58 +0900

    트랜지션 처리 및 스타일링

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <tool-bar/>
     <!-- 여기서 만약 url이 /news로 들어오면 아래 <router-view/>는 자동으로 마법처럼 <NewsView/>로 바뀐다. -->
-    <router-view/>
+    <transition name="page">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -14,8 +16,26 @@ export default {
 </script>
 
 <style>
+  a {
+    color: #34495e;
+    text-decoration: none;
+  }
+  a:hover {
+    color : #42b883;
+    text-decoration: underline;
+  }
+  a.router-link-exact-active {
+    text-decoration: underline;
+  }
   body {
     padding: 0;
     margin: 0;
+  }
+
+  .page-enter-active, .page-leave-active {
+    transition: opacity .5s;
+  }
+  .page-enter, .page-leave-to {
+    opacity: 0;
   }
 </style>
