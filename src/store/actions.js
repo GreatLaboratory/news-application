2817b2be9979bcaf3c808826979d5839566b96ea
Myung-gwan Kim <cartopia95@naver.com>
2019-09-18 22:17:14 +0900

    게시물정보, 사용자정보 vuex store 관리

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,9 @@
 import {
     fetchNewsList,
     fetchJobsList,
-    fetchAskList
+    fetchAskList,
+    fetchUserInfo,
+    fetchItemInfo,
 } from '../api'
 
 export default {
@@ -33,6 +35,24 @@ export default {
             .then(res=>{
                 commit('SET_ASK', res.data);
                 return res;
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    },
+    FETCH_USER({ commit }, userName) {
+        fetchUserInfo(userName)
+            .then(res=>{
+                commit('SET_USER', res.data)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    },
+    FETCH_ITEM({ commit }, itemId) {
+        fetchItemInfo(itemId)
+            .then(res=>{
+                commit('SET_Item', res.data)
             })
             .catch(err=>{
                 console.log(err)
