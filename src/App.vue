9c02d21178ccb00409dfc98b60261dc538309229
Myung-gwan Kim <cartopia95@naver.com>
2019-09-20 18:09:14 +0900

    스피너 추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
     <tool-bar/>
-    <!-- 여기서 만약 url이 /news로 들어오면 아래 <router-view/>는 자동으로 마법처럼 <NewsView/>로 바뀐다. -->
     <transition name="page">
+      <!-- 여기서 만약 url이 /news로 들어오면 아래 <router-view/>는 자동으로 마법처럼 <NewsView/>로 바뀐다. -->
       <router-view/>
     </transition>
+    <spinner :loading="loadingStatus"></spinner>
   </div>
 </template>
 
 <script>
 import ToolBar from "@/components/ToolBar";
+import Spinner from "@/components/Spinner";
+import bus from './utils/bus'
 export default {
-  components: {ToolBar}
+  components: {Spinner, ToolBar},
+  data() {
+    return {
+      loadingStatus : false
+    }
+  },
+  methods: {
+    startSpinner() {
+      this.loadingStatus = true;
+    },
+    endSpinner() {
+      this.loadingStatus = false;
+    }
+  },
+  created() {
+    bus.$on('start:spinner', this.startSpinner);
+    bus.$on('end:spinner', this.endSpinner);
+  },
+  beforeDestroy() {
+    bus.$off('start:spinner', this.startSpinner);
+    bus.$off('end:spinner', this.endSpinner);
+  }
 }
 </script>
 
